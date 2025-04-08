4a330e89fd5fba65f501df2754a7bff85ca5fae9
TrentDaka <130572502+TrentDaka@users.noreply.github.com>
2025-04-08 16:57:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript file for enhancing the web page functionality
+
+// Example: Smooth scrolling for anchor links (if any are added later)
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
